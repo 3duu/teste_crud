7cffba33abdba95dea67f04577968cd34b8848ba
Eduardo <eduu.porto@gmail.com>
2017-02-01 17:34:33 -0200

    Add JPA

--- crud/src/main/java/crud/ManageBean.java
+++ crud/src/main/java/crud/ManageBean.java
@@ -1,5 +1,6 @@
 package crud;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -8,29 +9,63 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 @ManagedBean
 public class ManageBean {
-	private Carro carro = new Carro();
-    private List<Carro> carros = new ArrayList<Carro>();
+	
+	private Costumer item;
+	private Costumer beforeEditItem;
+    private List<Costumer> list;
+    
+    //DAO
+    private CostumerDAO costumerDAO = new CostumerDAO();
 
-    public Carro getCarro() {
-        return carro;
-    }
 
-    public void setCarro(Carro carro) {
-        this.carro = carro;
-    }
-
-    public List<Carro> getCarros() {
-        return carros;
-    }
-
-    public void setCarros(List<Carro> carros) {
-        this.carros = carros;
+    private boolean editing;
+    
+    //@PostConstruct
+    public void init() {
+        list = new ArrayList<Costumer>();
     }
     
-
-    public void adicionar(){
-        if(this.carro != null)
-            this.carros.add(this.carro);
-        this.carro = new Carro();
+    public void add() {
+        // DAO save the add
+        item.setId(list.isEmpty() ? 1 : list.get(list.size() - 1).getId() + 1);
+        list.add(item);
+        item = new Costumer();
+    }
+    
+    public void resetAdd() {
+        item = new Costumer();
+    }
+    
+    public void edit(Costumer item) {
+        beforeEditItem = new Costumer(item.getId(), item.geName(), item.getCpf());
+        this.item = item;
+        editing = true;
+    }
+    
+    public void cancelEdit() {
+        //this.item.restore(beforeEditItem);
+        //this.item = new Student();
+        editing = false;
+    }
+    
+    public void saveEdit() {
+        this.item = new Costumer();
+        editing = false;
+    }
+    
+    public void delete(Costumer item) throws IOException {
+        list.remove(item);
+    }
+    
+    public List<Costumer> getList() {
+        return list;
+    }
+    
+    public Costumer getItem() {
+        return this.item;
+    }
+    
+    public boolean isEditing() {
+        return this.editing;
     }
 }
